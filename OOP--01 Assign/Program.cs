3e53c9b7e19e7bad15668f68621a9978783eec56
MohamedHisham393 <mohamed.hisham12393@gmail.com>
2026-09-22 23:39:51 +0300

    Theoretical Q1

--- OOP--01 Assign/Program.cs
+++ OOP--01 Assign/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            //Part 01 : Theoretical Questions
+
+            //Q1
+            // a) nothing happens to the original value because they will be two blocks in stack 
+            // b) changes in copied variable will appear in the original because they refer to the same addres in heap
         }
     }
 }
