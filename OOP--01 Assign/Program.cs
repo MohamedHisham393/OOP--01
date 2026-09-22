6a9ea982b590dbb0e735aa98b08daf22f5ce591d
MohamedHisham393 <mohamed.hisham12393@gmail.com>
2026-09-22 23:41:12 +0300

    Theoretical Q2

--- OOP--01 Assign/Program.cs
+++ OOP--01 Assign/Program.cs
@@ -9,6 +9,15 @@
             //Q1
             // a) nothing happens to the original value because they will be two blocks in stack 
             // b) changes in copied variable will appear in the original because they refer to the same addres in heap
+
+            //============================================================
+
+            //Q2
+            // a) 1- No validation on weight so if the user make weight = -10 it willnot prevent this
+            //    2- user can reach fields and modify on it using shipment.DeliveryFee
+            //    3- 
+
+            // b) using propeties --> (set) and make validation 
         }
     }
 }
